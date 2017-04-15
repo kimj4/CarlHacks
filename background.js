978e010b104c5a349d8fcc49193914d4cff5b857
Ju Yun <kimj4@carleton.edu>
2017-04-15 01:34:42 -0400

    some functionality for background

--- background.js
+++ background.js
@@ -1,8 +1,44 @@
-chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-    chrome.tabs.executeScript(tab.id, {"file": "a.js"});
-});
+setInterval(checkCurrent, 500); // run every second
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  console.log("in getCurrentTabUrl");
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.assert(typeof url == 'string', 'tab.url should be a string');
+    callback(url);
+  });
+}
 
 
+function checkCurrent() {
+  getCurrentTabUrl(function(url) {
+    chrome.storage.sync.get({
+      userUrls: ''
+    }, function(items) {
+      var goodUrls;
+      goodUrls = items.userUrls;
+      var goodUrlsArray = goodUrls.split('\n');
+
+      pathArray = url.split( '/' );
+      var protocol = pathArray[0];
+      var host = pathArray[2];
+      var homesite = host;
+
+      var i, ulen;
+      ulen = goodUrlsArray.length;
+      for (i = 0; i < ulen; i++) {
+        if (homesite == goodUrlsArray[i]) {
+        }
+      }
+    });
+  });
+}
 
 
 // document.addEventListener('DOMContentLoaded', function() {
