6f0b35c98c3e54e67029e4a3064fb90f62cd4d13
Ju Yun <kimj4@carleton.edu>
2017-04-15 00:36:36 -0400

    added basis option saving functionality

---
+++ optionsHandler.js
@@ -0,0 +1,28 @@
+function saveData() {
+    var urls = document.getElementById('tbox').value;
+    chrome.storage.sync.set({
+      userUrls: urls
+    }, function() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+    userUrls: ''
+  }, function(items) {
+    document.getElementById('tbox').value = items.userUrls;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    saveData);
