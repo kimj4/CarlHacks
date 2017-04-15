a44a416b970b35b5b9e9e1bffb22a8c6feabf34a
Emma Posega Rappleye <Emma Posega Rappleye>
2017-04-14 22:07:44 -0500

    modified popup

--- popup.js
+++ popup.js
@@ -16,6 +16,8 @@ function getCurrentTabUrl(callback) {
     currentWindow: true
   };
 
+  console.log("in getCurrentTabUrl");
+
   chrome.tabs.query(queryInfo, function(tabs) {
     // chrome.tabs.query invokes the callback with a list of tabs that match the
     // query. When the popup is opened, there is certainly a window and at least
@@ -66,6 +68,7 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
   var div = document.createElement("DIV");
   div.id = "someName";
@@ -74,11 +77,15 @@ document.addEventListener('DOMContentLoaded', function() {
   div.appendChild(img);
   document.body.appendChild(div);
   getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus("fuck");
+   renderStatus('Detecting ' + url);
 
-    }, function(errorMessage) {
-      renderStatus('fuck');
-    });
+
+   });
+ });
+
+document.addEventListener('DOMContentLoaded', function() {
+  //getCurrentTabUrl(function(url) {
+  getCurrentTabUrl(function(url) {
+    renderStatus("url");
   });
 });
