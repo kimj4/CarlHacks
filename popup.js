b28551b3ab2190d349a09594b5cffdc47e5ab457
Ju Yun <kimj4@carleton.edu>
2017-04-14 23:09:09 -0400

    did something

--- popup.js
+++ popup.js
@@ -70,15 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var div = document.createElement("DIV");
   div.id = "someName";
   var img = document.createElement("IMG");
-  img.src = "cat.png";
+  img.src = "/cat.png";
   div.appendChild(img);
   document.body.appendChild(div);
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus("fuck");
-
-    }, function(errorMessage) {
-      renderStatus('fuck');
-    });
-  });
 });
